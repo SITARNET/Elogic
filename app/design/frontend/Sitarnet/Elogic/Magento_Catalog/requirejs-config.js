8f1fe233858822bc50b3da905e4476791587a987
Alex Pupkin <admin@site.com>
2021-01-05 01:09:25 +0200

    No display my template

--- app/design/frontend/Sitarnet/Elogic/Magento_Catalog/requirejs-config.js
+++ app/design/frontend/Sitarnet/Elogic/Magento_Catalog/requirejs-config.js
@@ -1,14 +1,12 @@
 var config = {
-    config: {
-        map: {
-            '*': {
-                myCustomScript: 'Magento_Catalog/js/my-custom-script'
-            }
-        },
-        mixins: {
-            'Magento_Swatches/js/swatch-renderer': {
-                'Magento_Catalog/js/swatch-renderer-mixin': true
-            }
+    map: {
+        '*': {
+            myCustomScript: 'Magento_Catalog/js/my-custom-script'
+        }
+    },
+    mixins: {
+        'Magento_Swatches/js/swatch-renderer': {
+            'Magento_Catalog/js/swatch-renderer-mixin': true
         }
     }
 };
